3c247a7b41feaf23a361c3d526ba3728f234cbcb
Anna Welden <awelden@sacbee.com>
2016-07-21 17:30:16 +0000

    messing with deducer

---
+++ deducer.R
@@ -0,0 +1,11 @@
+head(Grocery.AMOS)
+names(Grocery.AMOS)
+
+cor(Grocery.AMOS[,5:ncol(Grocery.AMOS)])
+
+yum install libpng-devel
+
+install.packages("png")
+,"Deducer","DeducerExtras"
+library(JGR)
+JGR()
